7aeae376f254c9d4a73c9ca36559ad161a15b19b
nanooq <nanooq@nanooq.org>
2015-02-06 19:03:58 +0100

    added bash one liner to save arte stream taken from ubuntu users

---
+++ savestreamarte.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+rtmpdump -v -r "rtmp://artestras.fc.llnwd.net/artestras/s_artestras_scst_geoFRDE_de?s=1320220800&h=878865258ebb8eaa437b99c3c7598998" -o $HOME/Videos/ARTE-Live-Stream.mp4 
